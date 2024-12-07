f5cfbc4af5e485603a42c6fd90d0afee714b8555
Rahul V Ramesh <rahulvramesh@hotmail.com>
2024-12-07 11:34:58 +0700

    Initial Commit

---
+++ dotDNS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dotDNS
+//
+//  Created by Rahul VR on 07/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dotDNS/dotDNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotDNSApp.swift
+//  dotDNS
+//
+//  Created by Rahul VR on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct dotDNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
